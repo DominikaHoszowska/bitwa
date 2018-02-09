5266f0ae0e5f5ea6196b2195387be7416c982121
DominikaHoszowska <35043626+DominikaHoszowska@users.noreply.github.com>
2018-02-09 18:59:29 +0100

    wczytywanie

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "bitwa/Gracz.h"
 #include "bitwa/Gra.h"
-
+using std::vector;
 bool sprawdzDlugoscLinii(unsigned int dlugoscLinii)
 {
     if(dlugoscLinii>0 && dlugoscLinii%2==0)
@@ -20,45 +20,48 @@ bool sprawdzMaksymalnaLiczbeTur(int liczbaTur)
     return false;
 }
 int main() {
-    int dlugoscLinii=6;
-    int liczbaTur=100;
+    int dlugoscLinii;
+    int liczbaTur;
 
-//    std::cout << "Wprowadz dlugosc linii" << std::endl;
-//    std::cin >> dlugoscLinii;
-//
-//    while (!sprawdzDlugoscLinii(dlugoscLinii))
-//    {
-//        std::cout << "Wprowadz dodatnia liczbe parzysta oznaczajaca dlugosc linii" << std::endl;
-//        std::cin >> dlugoscLinii;
-//    }
-//    std::cout << "Wprowadz maksymalna liczbe tur" << std::endl;
-//    std::cin >> liczbaTur;
-//    while (!sprawdzMaksymalnaLiczbeTur(liczbaTur))
-//    {
-//        std::cout << "Wprowadz dodatnia liczbe oznaczajaca maksymalna liczbe tur" << std::endl;
-//        std::cin >> liczbaTur;
-//    }
+    std::cout << "Wprowadz dlugosc linii" << std::endl;
+    std::cin >> dlugoscLinii;
+
+    while (!sprawdzDlugoscLinii(dlugoscLinii))
+    {
+        std::cout << "Wprowadz dodatnia liczbe parzysta oznaczajaca dlugosc linii" << std::endl;
+        std::cin >> dlugoscLinii;
+    }
+    std::cout << "Wprowadz maksymalna liczbe tur" << std::endl;
+    std::cin >> liczbaTur;
+    while (!sprawdzMaksymalnaLiczbeTur(liczbaTur))
+    {
+        std::cout << "Wprowadz dodatnia liczbe oznaczajaca maksymalna liczbe tur" << std::endl;
+        std::cin >> liczbaTur;
+    }
 
     /*Wczytanie wojsk*/
-    std::vector<std::vector<std::vector<char>>> tabPole /*(6)*/ =
-            {{{'K', 'T','T','T','T','K'},{'L', 'L','B','B','L','L'},{'K', 'H','M', 'M','H', 'K'}},
-             {{'H', 'M','K', 'K','M', 'H'},{'L', 'B','k', 'k','M', 'H'},{'T', 'T','T', 'T','T', 'X'}}};
-//    char znak;
-//    std::cout<<"Wprowadz wojska";
-//  for(int nrWiersza=2;nrWiersza>=0;nrWiersza--)
-//  {
-//      for(unsigned int i=0;i<dlugoscLinii;i++)
-//      {
-//          tabPole.at(0).at(nrWiersza).at(i)=znak;
-//      }
-//  }
-//    for(int nrWiersza=0;nrWiersza<=2;nrWiersza++)
-//    {
-//        for(unsigned int i=0;i<dlugoscLinii;i++)
-//        {
-//            tabPole.at(1).at(nrWiersza).at(i)=znak;
-//        }
-//    }
+    std::vector<std::vector<std::vector<char>>> tabPole(2,vector<vector<char>>(3));
+    char znak;
+    std::cout<<"Wprowadz wojska";
+  for(int nrWiersza=2;nrWiersza>=0;nrWiersza--)
+  {
+      for( int i=0;i<dlugoscLinii;i++)
+      {
+          std::cin>>znak;
+          tabPole.at(0).at(nrWiersza).push_back(znak
+
+          );
+      }
+  }
+    for(int nrWiersza=0;nrWiersza<=2;nrWiersza++)
+    {
+        for( int i=0;i<dlugoscLinii;i++)
+        {
+
+            std::cin>>znak;
+            tabPole.at(1).at(nrWiersza).push_back(znak);
+        }
+    }
     Gra gra(liczbaTur,dlugoscLinii,tabPole);
     gra.wypisz();
     while(!gra.czyKoniec())
